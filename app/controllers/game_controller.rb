03234165b55cec7d8102aaeef2cd7187c0666694
john Caropino <jccaropino7786@live.com>
2023-04-06 21:36:22 -0700

    new commit

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -2,4 +2,23 @@ class GameController < ApplicationController
     def index
         @board = Board.last || Board.create(title: 'Game', state: ['']*9)
       end
+
+      def play
+        board = Board.last
+        state = board.state
+        i, j = params[:i].to_i, params[:j].to_i
+    
+        # check if move is valid
+        if state[i*3 + j] == ''
+          # update board state with player's move
+          state[i*3 + j] = (board.title == 'Game') ? 'X' : 'O'
+          board.update(state: state)
+    
+          # switch to other player's turn
+          board = Board.create(title: (board.title == 'Game') ? 'Player 2' : 'Game', state: state)
+        end
+    
+        redirect_to game_index_path
+      end
+      
 end
